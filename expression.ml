864cb708077a08a727886fc194ecaf2bf2d3e7b1
eric5512 <ericdandria2@gmail.com>
2021-06-27 19:47:19 +0200

    first commit

---
+++ expression.ml
@@ -0,0 +1,28 @@
+type operation = Add of operation * operation
+                | Sub of operation * operation
+                | Mul of operation * operation
+                | Div of operation * operation
+                | Pow of operation * operation
+                | Var of string
+                | Val of float;;
+
+
+let rec eval env = function
+                | Add (l, r) -> (eval env l) +. (eval env r)
+                | Sub (l, r) -> (eval env l) -. (eval env r)
+                | Mul (l, r) -> (eval env l) *. (eval env r)
+                | Div (l, r) -> (eval env l) /. (eval env r)
+                | Pow (l, r) -> Float.pow (eval env l) (eval env r)
+                | Var s -> Hashtbl.find env s
+                | Val v -> v;;
+
+let split_on_2 str chr = let i = String.index str chr in 
+                        (String.sub str 0 i, String.sub str (i+1) ((String.length str)-i-1));;
+
+let rec parse str = if      String.contains str '-' then let (l, r) = split_on_2 str '-' in Sub (parse l, parse r)
+                    else if String.contains str '+' then let (l, r) = split_on_2 str '+' in Add (parse l, parse r)
+                    else if String.contains str '*' then let (l, r) = split_on_2 str '*' in Mul (parse l, parse r)
+                    else if String.contains str '/' then let (l, r) = split_on_2 str '/' in Div (parse l, parse r)
+                    else if String.contains str '^' then let (l, r) = split_on_2 str '^' in Pow (parse l, parse r)
+                    else let f = Float.of_string_opt str in if f = None then Var str
+                    else Val (Float.of_string str);;
